4e4b74a927b7525c195618db3b47ff0ebf0eb6b9
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-02-07 16:58:17 -0400

    inicializado HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const movieURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -10,13 +11,24 @@ const Home = () => {
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
+
+    setTopMovies(data.results);
   };
 
   useEffect(() => {
     const TopRatedUrl = `${movieURL}top_rated?${apiKey}`;
     getTopRatedMovies(TopRatedUrl);
   }, []);
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
